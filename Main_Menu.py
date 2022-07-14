aeca3b068f890f31c59451ea34d4d7e360849b2b
Jah's Dalmation <ghost@tokyo.hub>
2022-07-14 15:14:04 +1200

    ...

--- Main_Menu.py
+++ Main_Menu.py
@@ -2,7 +2,6 @@
 from Data_Checks import *
 
 def Main_Menu():
-<<<<<<< HEAD
 
     # Loop the Main Menu to ensure that a mode is chosen succesfully
     #   before the function ends.
@@ -48,30 +47,3 @@ def Main_Menu():
                 #Mode = modes[int(User_Mode)]
                 #Deciding = False
                 #return Mode
-=======
-    modes = ['Local', 'External', 'Quit']
-
-    mode_count = 0
-    for mode in modes:
-        print('[ %s ] > [ %s ]' % (mode_count, mode))    
-        mode_count += 1
-
-    User_Mode = input('[ X ] > ')
-
-    menu = int_check(User_Mode)
-    if menu is False:
-        print('FYM')
-        return False
-    menu = range_check(User_Mode, 0, mode_count)
-    if menu is False:
-        print('FYM')
-        return False
-    elif menu:
-        Mode = modes[int(User_Mode)]
-        print(Mode)
-        return Mode
-
-No = True
-while No:
-    Main_Menu()
->>>>>>> 7519a2c (Initial import of program files!)
